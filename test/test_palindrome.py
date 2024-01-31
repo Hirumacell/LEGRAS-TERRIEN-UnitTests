77da09972074bf601f6e398b29d059f177ce6c0d
victor <victor.terrien@epsi.fr>
2024-01-31 16:45:43 +0100

    [RED] - Test étape 1

---
+++ test/test_palindrome.py
@@ -0,0 +1,53 @@
+import os
+import unittest
+
+class UnitTest(unittest.TestCase):
+
+    def test_mirror(self):
+        # ETANT DONNE une chaîne
+        chain = "test"
+
+        # QUAND on saisit une chaîne
+        result = PalindormeDetect(chain)
+
+        # ALORS celle-ci est renvoyée en miroir
+        want = chain[::-1]
+        self.assertIn(want, result)
+
+
+    def test_palindrome(self):
+        # ETANT DONNE une chaîne
+        chain = "kayak"
+
+        # QUAND on saisit un palindrome
+        result = PalindromeDetect(chain)
+
+        # ALORS celui-ci est renvoyé ET "Bien dit" est envoyé ensuite
+        expected = chain + os.linesep + "Bien dit!"
+        self.assertIn(expected, result)
+
+
+    def test_helloInFirst(self):
+        # ETANT DONNE une chaîne
+        chain = "test"
+        expected = "Bonjour"
+
+        # QUAND on saisit une chaîne
+        result = PalindormeDetect(chain)
+
+        # ALORS "Bonjour" est envoyé avant toute réponse
+        firstLine = result.split(os.linesep)[0]
+        self.assertEqual(firstLine, expected)
+
+
+    def test_GoodbyeInLast(self):
+        # ETANT DONNE une chaîne
+        chain = "test"
+        expected = "Au revoir"
+
+        # QUAND on saisit une chaîne
+        result = PalindormeDetect(chain)
+
+        # ALORS "Au revoir" est envoyé en dernier
+        lastLine = result.split(os.linesep)[1]
+        self.assertEqual(lastLine, expected)
