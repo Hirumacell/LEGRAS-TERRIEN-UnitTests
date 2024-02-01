1957a0276ecdba7cdda4d0270583ced9d3df7a86
victor <victor.terrien@epsi.fr>
2024-02-01 12:49:43 +0100

    [REFORMAT] - Test étape 2 failed

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -19,41 +19,54 @@ class UnitTest(unittest.TestCase):
 
 
     def test_palindrome(self):
-        # ETANT DONNE une chaîne
-        chain = "kayak"
+        parameters = [[FrenchLanguage(), "Bien dit !"], [EnglishLanguage(), "Well said !"]]
 
-        # QUAND on saisit un palindrome
-        result = PalindromeDetect.detect(chain)
+        for param in parameters:
+            with self.subTest(param[0]):
+                # ETANT DONNE une chaîne ET l'utilisateur parle une langue
+                chain = "kayak"
+                language = param[0]
 
-        # ALORS celui-ci est renvoyé ET "Bien dit" est envoyé ensuite
-        expected = chain + os.linesep + "Bien dit!"
-        self.assertIn(expected, result)
+                # QUAND on saisit un palindrome
+                result = PalindromeDetect.choosenLanguage(language).detect(chain)
+
+                # ALORS celui-ci est renvoyé ET <Bien dit> de cette langue est envoyé
+                expected = chain + os.linesep + param[1]
+                self.assertIn(expected, result)
 
 
     def test_helloInFirst(self):
-        # ETANT DONNE une chaîne
-        chain = "test"
-        expected = "Bonjour"
+        parameters = [[FrenchLanguage(), "Bonjour"], [EnglishLanguage(), "Hello"]]
 
-        # QUAND on saisit une chaîne
-        result = PalindromeDetect.detect(chain)
+        for param in parameters:
+            with self.subTest(param[0]):
+                # ETANT DONNE une chaîne ET l'utilisateur parle une langue
+                chain = "test"
+                language = param[0]
 
-        # ALORS "Bonjour" est envoyé avant toute réponse
-        firstLine = result.split(os.linesep)[0]
-        self.assertEqual(firstLine, expected)
+                # QUAND on saisit une chaîne
+                result = PalindromeDetect.choosenLanguage(language).detect(chain)
+
+                # ALORS <Bonjour> de cette langue est envoyé avant toute réponse
+                firstLine = result.split(os.linesep)[0]
+                self.assertEqual(firstLine, param[1])
 
 
     def test_GoodbyeInLast(self):
-        # ETANT DONNE une chaîne
-        chain = "test"
-        expected = "Au revoir"
+        parameters = [[FrenchLanguage(), "Au revoir"], [EnglishLanguage(), "Goodbye"]]
 
-        # QUAND on saisit une chaîne
-        result = PalindromeDetect.detect(chain)
+        for param in parameters:
+            with self.subTest(param[0]):
+                # ETANT DONNE une chaîne ET l'utilisateur parle une langue
+                chain = "test"
+                language = param[0]
 
-        # ALORS "Au revoir" est envoyé en dernier
-        lastLine = result.split(os.linesep)[-1]
-        self.assertEqual(lastLine, expected)
+                # QUAND on saisit une chaîne
+                result = PalindromeDetect.choosenLanguage().detect(chain)
+
+                # ALORS <Au revoir> de cette langue est envoyé en dernier
+                lastLine = result.split(os.linesep)[-1]
+                self.assertEqual(lastLine, param[1])
 
 if __name__ == '__main__':
     unittest.main()
